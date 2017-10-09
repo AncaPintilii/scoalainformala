322d4d96571d99f3897010d74af3cd0616ca2e86
AncaPintilii <pintilii.anca@gmail.com>
2017-10-09 12:36:37 +0300

    upload

---
+++ exercitii-curs/ciorna.js
@@ -0,0 +1,10 @@
+//functie care calculeaza daca un numar e par sau impar
+function isPar (a) {
+    if(a%2 == 0) {
+    return true;}
+    else {
+    return false;}
+    }
+    isPar(3);
+    isPar(6);
+    
