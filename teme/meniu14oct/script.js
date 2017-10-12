7dc183f66260f44f45b683cfe5eb83cc96041d51
AncaPintilii <pintilii.anca@gmail.com>
2017-10-12 20:30:11 +0300

    upload

--- teme/meniu14oct/script.js
+++ teme/meniu14oct/script.js
@@ -9,8 +9,21 @@ function afiseazaProduse() {
     xhttp.onreadystatechange = function () { //se specifica comportamentul daca primim 200
         if (this.readyState == 4 && this.status == 200) { //aici incepe tratamentul raspunsul de la server ->
             var json = this.responseText;
-            console.log(json); 
+            console.log(json);
             gJson = json;
+
+            var image = JSON.parse(json).weather[0].icon;
+            var sufix =".png";
+            document.getElementById("poza").src = URL_WEATHER_ICON_PREFIX + image + sufix; 
+
+            /*var nume = JSON.parse(json).weather[0].description;
+            document.getElementById("descriere").innerHTML = descriereAcum;
+
+            var ingrediente = JSON.parse(json).weather[0].description;
+            document.getElementById("descriere").innerHTML = descriereAcum;
+            
+            buton detalii*/
+
         }
     };
     xhttp.open("GET", MENU_ITEM_SERVER_URL + search, true); //true - async, false - sync
