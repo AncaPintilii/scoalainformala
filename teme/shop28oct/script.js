c40dd4738456d7ba7dd5b44413be17c38b608eac
AncaPintilii <pintilii.anca@gmail.com>
2017-11-11 01:03:16 +0200

    var 1: doar baza; var 2: tabel cu undefined

--- teme/shop28oct/script.js
+++ teme/shop28oct/script.js
@@ -1,10 +1,3 @@
-//var COFFEE_SERVER_URL = "https://cotroccino.firebaseio.com/.json";
-//var COFFEE_ITEM_SERVER_URL = "https://cotroccino.firebaseio.com/produse/";
-//var gJson;
-
-//var coffee;
-//var coffee_list = [];
-
 class COFFEE {
     constructor(id, description, img, name, price, quantity) {
         this.id = id;
@@ -15,7 +8,6 @@ class COFFEE {
         this.quantity = quantity;
     }
 }
-
 /* start carousel */
 var slides = document.querySelectorAll('#slides .slide');
 var currentSlide = 0;
@@ -28,12 +20,63 @@ function nextSlide() {
 }
 /* stop carousel */
 
-`<td style="width: 33%">
-    <p>${name}</p>
-    <img src="${img}" />
-        <p>${price}</p>
-        <p>${description}</p>
-        <a href="./details.html">
-            <button>DETAILS</button>
-        </a>
+/*var xhttp = new XMLHttpRequest(); -> no tabel, doar baza de date
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    document.getElementById("coffees").innerHTML =
+    this.responseText;
+  }
+};
+xhttp.open("GET", "https://cotroccino.firebaseio.com/produse/.json", true);
+xhttp.send();
+var str = "<tr>";
+
+function draw(coffees) {
+var list = Object.keys(coffees);
+for (var i = 0; i < list.length; i++) {
+  str += `<td style="width: 33%">
+  <p>${list[i].name}</p>
+  <img src="${list[i].img}" />
+      <p>${list[i].price}</p>
+      <p>${list[i].description}</p>
+      <a href="./details.html">
+          <button>DETAILS</button>
+      </a>
 </td>`
+}
+str += "</tr>";
+document.querySelector("#coffees table").innerHTML = str;
+}
+/////////////////////////////////////////////////////////////////
+/* tabel cu undefined
+var xhttp = new XMLHttpRequest();  
+xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        window.coffees = JSON.parse(xhttp.responseText)
+        draw(coffees);
+
+    }
+};
+xhttp.open("GET", "https://cotroccino.firebaseio.com/produse.json", true);
+xhttp.send();
+
+
+function draw(coffees) {
+
+    var str = "<tr>";
+
+    var list = Object.keys(coffees);
+    for (var i = 0; i < list.length; i++) {
+        str += `<td style="width: 33%">
+		<p>${list[i].name}</p>
+		<img src="${list[i].img}" />
+			<p>${list[i].price}</p>
+			<p>${list[i].description}</p>
+			<a href="./details.html">
+				<button>DETAILS</button>
+			</a>
+	</td>`
+    }
+    str += "</tr>";
+    document.querySelector("#coffees table").innerHTML = str;
+}*////////////////////////////
