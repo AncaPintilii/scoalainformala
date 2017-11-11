0fab02d265a69fbd7d03075ca68b587a8caee50b
AncaPintilii <pintilii.anca@gmail.com>
2017-11-11 10:32:50 +0200

    xmlhttp request

--- teme/shop28oct/script.js
+++ teme/shop28oct/script.js
@@ -19,47 +19,15 @@ function nextSlide() {
     slides[currentSlide].className = 'slide showing';
 }
 /* stop carousel */
-
-/*var xhttp = new XMLHttpRequest(); -> no tabel, doar baza de date
-xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    document.getElementById("coffees").innerHTML =
-    this.responseText;
-  }
-};
-xhttp.open("GET", "https://cotroccino.firebaseio.com/produse/.json", true);
-xhttp.send();
-var str = "<tr>";
-
-function draw(coffees) {
-var list = Object.keys(coffees);
-for (var i = 0; i < list.length; i++) {
-  str += `<td style="width: 33%">
-  <p>${list[i].name}</p>
-  <img src="${list[i].img}" />
-      <p>${list[i].price}</p>
-      <p>${list[i].description}</p>
-      <a href="./details.html">
-          <button>DETAILS</button>
-      </a>
-</td>`
-}
-str += "</tr>";
-document.querySelector("#coffees table").innerHTML = str;
-}
-/////////////////////////////////////////////////////////////////
-/* tabel cu undefined
-var xhttp = new XMLHttpRequest();  
+var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         window.coffees = JSON.parse(xhttp.responseText)
         draw(coffees);
-
     }
 };
-xhttp.open("GET", "https://cotroccino.firebaseio.com/produse.json", true);
+xhttp.open("GET", "https://cotroccino.firebaseio.com/produse/.json", true);
 xhttp.send();
-
 
 function draw(coffees) {
 
@@ -67,16 +35,21 @@ function draw(coffees) {
 
     var list = Object.keys(coffees);
     for (var i = 0; i < list.length; i++) {
+        var coffee = coffees[list[i]];
         str += `<td style="width: 33%">
-		<p>${list[i].name}</p>
-		<img src="${list[i].img}" />
-			<p>${list[i].price}</p>
-			<p>${list[i].description}</p>
+		<p>${coffee.name}</p>
+		<img src="${coffee.img}" />
+			<p>${coffee.price}</p>
+			<p>${coffee.description}</p>
 			<a href="./details.html">
 				<button>DETAILS</button>
 			</a>
-	</td>`
+    </td>`
+        if (i %3 == 2) {
+            str += ` </tr>
+            <tr>`
+        }
     }
     str += "</tr>";
     document.querySelector("#coffees table").innerHTML = str;
-}*////////////////////////////
+}
