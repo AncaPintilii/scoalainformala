deccfba0752ba826daf6268973dc3aa7c04c0795
AncaPintilii <pintilii.anca@gmail.com>
2018-01-09 23:12:30 +0200

    upload

--- teme/shop28oct/cart.js
+++ teme/shop28oct/cart.js
@@ -1,4 +1,4 @@
-/* Am nevoie de o functie care:
+/* Am nevoie de o functie care sa:
 1. deseneze tabelul
 2. urce comanda fiecaruia
 3. sa poata sterge produse din cart
@@ -35,7 +35,7 @@ function createCart(cart) {
 
     var str2 = JSON.stringify(cart);
 //////////////de modificat de aici incolo
-    var list = Object.keys(cart);
+    /*var list = Object.keys(cart);
     for (var i = 0; i < list.length; i++) {
         var coffee = cart[list[i]];
         str += `<td><button onclick='change(${JSON.stringify(coffee)},${i})'>${coffee.name}</button></td>
@@ -49,5 +49,26 @@ function createCart(cart) {
     }
     str += "</tr></tbody>";
     document.querySelector("#cart table").innerHTML = str;
-}
+}*/
+
+
+
+
+
+
 /*///////////// stop creare tabel ///////////*/
+
+/*///////////// start update cart ///////////*/
+/*///////////// stop update cart ///////////*/
+
+/*///////////// start delete item de tot ///////////*/
+/*///////////// stop delete item de tot ///////////*/
+
+/*///////////// start add quantity ///////////*/
+/*///////////// stop add quantity ///////////*/
+
+/*///////////// start substract quantity ///////////*/
+/*///////////// stop substract quantity ///////////*/
+
+/*///////////// start delete all cart ///////////*/
+/*///////////// stop delete all cart ///////////*/
--- teme/shop28oct/details.js
+++ teme/shop28oct/details.js
@@ -1,11 +1,13 @@
-function addToCart() {
+/*///////////// start confirmare buton Add To Cart ///////////*/
+function addToCartConfirmarion() {
     document.getElementById("idAddToCart_div").style.display = "block";
-    
+
     setTimeout(function () {
         document.getElementById("idAddToCart_div").style.display = "none";
     }, 2000)
 }
-
+/*///////////// stop confirmare buton Add To Cart ///////////*/
+///////////// start preluare json ///////////*/
 var idprodus = window.location.search.substring(10); /* doar 1, nu ?idProdus=1 */
 
 var xhttp = new XMLHttpRequest();
@@ -15,13 +17,42 @@ xhttp.onreadystatechange = function () {
         draw(coffee);
     }
 };
-xhttp.open("GET", "https://cotroccino.firebaseio.com/produse/"+idprodus+".json", true);
+xhttp.open("GET", "https://cotroccino.firebaseio.com/produse/" + idprodus + ".json", true);
 xhttp.send();
-
-function draw (cafea) {
-    document.getElementById("img").src=cafea.img;
-    document.getElementById("name").innerHTML=cafea.name;
-    document.getElementById("description").innerHTML=cafea.description;
-    document.getElementById("price").innerHTML=cafea.price;
-    document.getElementById("stock").innerHTML=cafea.stock;
+/*///////////// stop preluare json ///////////*/
+///////////// start creare "tabel" detalii ///////////*/
+function draw(cafea) {
+    document.getElementById("img").src = cafea.img;
+    document.getElementById("name").innerHTML = cafea.name;
+    document.getElementById("description").innerHTML = cafea.description;
+    document.getElementById("price").innerHTML = cafea.price;
+    document.getElementById("stock").innerHTML = cafea.stock;
 }
+///////////// stop creare "tabel" detalii ///////////*/
+///////////// start add to cart ///////////*/
+class userChoice {
+    constructor(id, nameItem, priceItem, quantityItem, subtotalItem) {
+        this.id = id;
+        this.name = nameItem;
+        this.price = priceItem;
+        this.quantity = quantityItem;
+        this.subtotal = subtotalItem;
+    }
+}
+function addToCart() {
+    var addToCartName = document.getElementById("name").innerHTML;
+    var addToCartPrice = parseInt(document.getElementById("price").innerHTML);
+    var addtoCartQuantity = parseInt(document.getElementById("coffee_quantity").value);
+    var subTotal = addtoCartQuantity * addToCartPrice;
+    var itemId = document.getElementById("id").value; //eroare aici, value of null
+    var userItem = new userChoice(addToCartName, addToCartPrice, addtoCartQuantity, subTotal, itemId);
+    
+
+    if (addtoCartQuantity > 0 && addtoCartQuantity <= parseInt(document.getElementById("stock").innerHTML)) {
+        updateFirebaseUser ("https://cotroccino.firebaseio.com/produse.json, userItem");
+   }
+   else {
+       alert("Too much items for our stock!");
+   }
+}
+///////////// stop add to cart ///////////*/
